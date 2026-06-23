b61bd13dba33d38f0c74d2345ebc7b5016e693bd
Vishal Vaibhav <madpackerz@gmail.com>
2026-06-23 21:06:48 +0530

    a2a-probe 0.2.4

--- Formula/a2a-probe.rb
+++ Formula/a2a-probe.rb
@@ -2,16 +2,16 @@ class A2aProbe < Formula
   desc "Command-line client for the A2A (Agent-to-Agent) Protocol"
   homepage "https://github.com/CodeYogiCo/a2a-probe"
   license "MIT"
-  version "0.2.3"
+  version "0.2.4"
 
   on_macos do
     url "https://github.com/CodeYogiCo/a2a-probe/releases/download/v#{version}/a2a-probe-darwin-arm64"
-    sha256 "f48f5e29296258b385e1905ec743e293a98b2af0b5b4192624552d798c0cdc05"
+    sha256 "f5c0adf8d103c7bd81a7ad54717ce028ba74be24cfa633ed818d2d1a4cc2a402"
   end
 
   on_linux do
     url "https://github.com/CodeYogiCo/a2a-probe/releases/download/v#{version}/a2a-probe-linux-amd64"
-    sha256 "762e28eef18f4e0240480beea498c9290274d2b51c5ae6fdef847c5409c8f8a9"
+    sha256 "0a8703e8d2353019111d996d4a4d9ee8f89e5daa76115671a88dad9618707342"
   end
 
   def install
